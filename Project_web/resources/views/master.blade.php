8e0721e76f15fb2b38567e3b43c78899f6ef6794
Cédric <cedric.mevel@viacesi.fr>
2019-01-21 16:33:32 +0100

    Minor change

--- Project_web/resources/views/master.blade.php
+++ Project_web/resources/views/master.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>BDE Bordeaux</title>
-        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/app.css" rel="stylesheet">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
