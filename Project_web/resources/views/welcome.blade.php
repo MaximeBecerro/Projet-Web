fd610a37560f829d5629d163afc3252523d23e36
Maxime <maximebecerro@gmail.com>
2019-01-21 10:07:39 +0100

    Test title 2

--- Project_web/resources/views/welcome.blade.php
+++ Project_web/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    BDE Bordeaux
                 </div>
 
                 <div class="links">
