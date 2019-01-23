56e5f500a2f810cb486a8a8e56d5b6464da52ae8
Cédric <cedric.mevel@viacesi.fr>
2019-01-23 13:09:14 +0100

    Modification carousel

--- Project_web/resources/views/master.blade.php
+++ Project_web/resources/views/master.blade.php
@@ -28,7 +28,7 @@
            </div>
         </header>
 
-        <nav class="navbar  navwidht navbar-collapse-xs navbar-toggler navbar-expand-lg" id = "navbar">
+        <nav class="navbar navwidht navbar-collapse-xs navbar-toggler navbar-expand-lg " id = "navbar">
          @yield('navbar')
          <a class="navbar-brand text mx-auto" href="#" ></a>
 
@@ -42,7 +42,6 @@
            <li class="nav-item">
              <a class="nav-link text" href="/idees">Boite à idées</a>
            </li>
-           
            <li class="nav-item">
                 <a class="nav-link text" href="/boutique">Boutique</a>
             </li>
@@ -113,15 +112,9 @@
         test footer
         <img src="{{ URL::to('/images/oui.jpg') }}">
     </footer> 
-
-
-
-
-
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-
     </body>
    
 </html>
--- Project_web/resources/views/welcome.blade.php
+++ Project_web/resources/views/welcome.blade.php
@@ -7,28 +7,66 @@
 
   <div>
 
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div id="carousel" class="carousel slide sizecarousel" data-ride="carousel">
+
+        <ol class="carousel-indicators">
+            <li data-target="#carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel" data-slide-to="1"></li>
+            <li data-target="#carousel" data-slide-to="2"></li>
+            <li data-target="#carousel" data-slide-to="3"></li>
+          </ol>
+
       <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img class="d-block w-100" src="{{ URL::to('/images/logo.png') }}" alt="First slide">
+        <div class="carousel-item active" >
+          <img class="block w-50" src="{{ URL::to('/images/logo.png') }}" alt="First slide" >
+          <div class="carousel-caption d-none d-md-block descriptionC">
+            <h5>Venez voir notre boutique de goodies</h5>
+          </div>    
         </div>
         <div class="carousel-item">
-          <img class="d-block w-100" src="{{ URL::to('/images/logo.png') }}" alt="Second slide">
+          <img class="block w-50" src="{{ URL::to('/images/logo.png') }}" alt="Second slide">
+          <div class="carousel-caption d-none d-md-block descriptionC">
+            <h5>Venez voir notre boutique de goodies </h5>
+          </div>  
         </div>
         <div class="carousel-item">
-          <img class="d-block w-100" src="{{ URL::to('/images/logo.png') }}" alt="Third slide">
+          <img class="block w-50" src="{{ URL::to('/images/logo.png') }}" alt="Third slide">
+          <div class="carousel-caption d-none d-md-block descriptionC">
+            <h5>Venez voir notre boutique de goodies </h5>
+          </div>  
         </div>
+
+        <div class="carousel-item">
+          <img class="block w-50" src="{{ URL::to('/images/logo.png') }}" alt="Fourth slide">
+          <div class="carousel-caption d-none d-md-block descriptionC">
+           <h5>Évènement à venir </h5>
+          </div>  
+        </div>
+        <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
+
+    <script>
+          $('carousel').carousel({
+        interval: 1;
+      })  
+    </script>
+  
     </div>
 
   </div>
+
+  <div class="description">
+    Description du BDE du CESI de Bordeaux avec une histoire et tout
+  </div>
 @stop
+
+
+
 
