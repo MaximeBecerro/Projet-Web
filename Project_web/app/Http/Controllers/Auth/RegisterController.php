b35a26e20085aa9f0f61ef3dc7ab350c5a93743c
Yannis Taibi <yannis.taibi@viacesi.fr>
2019-01-23 20:26:11 +0100

    Centers in register

--- Project_web/app/Http/Controllers/Auth/RegisterController.php
+++ Project_web/app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
+            'center' => ['required']
         ]);
     }
 
@@ -66,7 +67,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'centerid' => $data['centerid'],
+            'centerid' => $data['centre'],
         ]);
     }
 
--- Project_web/resources/views/auth/register.blade.php
+++ Project_web/resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nom') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
@@ -27,7 +27,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
@@ -41,17 +41,18 @@
                         </div>
 
                         <div class="form-group row">
-                                <label for="centre" class="col-md-4 col-form-label text-md-right">{{ __('Centre') }}</label>
-                                <select class="form-control">
+                                <label for="center" class="col-md-4 col-form-label text-md-right">{{ __('Centre') }}</label>
+                                <select class="form-control{{ $errors->has('center') ? ' is-invalid' : '' }}" style="text-transform:capitalize" name="center" value="{{ old('center') }}" required>
+                                   <option value="">Choisissez un Centre</option>
                                     @foreach ($centers as $center)
-                                        <option value="{{$center->CenterID}}">{{$center->CenterName}}</option>
+                                        <option value="{{$center->centerid}}">{{$center->CenterName}}</option>
                                     @endforeach
                                     </select>
                                 </div>
                             </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Mot de passe') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
@@ -65,7 +66,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmez le Mot de passe') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
