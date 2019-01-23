4966771162374b4ef03d531763ecf46775d3bcee
Yannis Taibi <yannis.taibi@viacesi.fr>
2019-01-23 10:40:03 +0100

    Revert "Revert "Fixes""
    This reverts commit f133ca43d9d276df7eb30b942add1864ab142214.

--- Project_web/resources/views/auth/register.blade.php
+++ Project_web/resources/views/auth/register.blade.php
@@ -1,5 +1,4 @@
 @extends('master')
-@extends('layouts.app')
 
 
 @section('content')
--- Project_web/resources/views/idees.blade.php
+++ Project_web/resources/views/idees.blade.php
@@ -1,6 +1,6 @@
 @extends('master')
 
-@section('title')
+@section('content')
 TELLEMENT D'IDEES WTF
 <ul class="list">
         @foreach ($centers as $center)
