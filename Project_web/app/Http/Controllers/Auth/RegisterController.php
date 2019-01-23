c652aaee7ea1fe9c778d6013c19a976a763d9c0d
Yannis Taibi <yannis.taibi@viacesi.fr>
2019-01-23 17:20:46 +0100

    database changes

--- Project_web/app/Http/Controllers/Auth/RegisterController.php
+++ Project_web/app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'centerid' => $data['centerid'],
         ]);
     }
 
