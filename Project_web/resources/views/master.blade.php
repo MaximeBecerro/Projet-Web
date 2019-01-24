8302058f1fe87922147025c4d1df1aab215b2145
Cédric <cedric.mevel@viacesi.fr>
2019-01-24 14:35:31 +0100

    Auto stash before merge of "Branch1" and "origin/Branch1"

--- Project_web/resources/views/master.blade.php
+++ Project_web/resources/views/master.blade.php
@@ -1,33 +1,33 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>BDE Bordeaux</title>
-        <link href="css/app.css" rel="stylesheet">
+  <title>BDE Bordeaux</title>
+  <link href="css/app.css" rel="stylesheet">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
-        <!-- Styles -->
-        {{HTML::style("css/style.css")}}
+  <!-- Styles -->
+  {{HTML::style("css/style.css")}}
         
 
-    </head>
-    <body>
-        <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
-        <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
-        @yield('cookies')
-        <script>
-        window.addEventListener("load", function(){
-          "palette": {
-            "popup": {
-        window.cookieconsent.initialise({
-            },
+</head>
+  <body>
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
+    <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
+    @yield('cookies')
+    <script>
+    window.addEventListener("load", function(){
+      "palette": {
+        "popup": {
+    window.cookieconsent.initialise({
+        },
               "background": "#000"
             "button": {
               "background": "#f1d600"
@@ -42,18 +42,15 @@
         </script>
         <header class="header title flex-center row " >
             <div class="col-md-1.5"> 
-                    <img src="{{ URL::to('/images/logo.png') }} ">
+              <img src="{{ URL::to('/images/logo.png') }} ">
             </div>
             <div class="col-xs-2 col-md-6.5 flex-center"> 
-               
-           Bureau des élèves CESI Bordeaux
+              Bureau des élèves CESI Bordeaux
            </div>
         </header>
 
-        <nav class="navbar navwidht navbar-collapse-xs navbar-toggler navbar-expand-lg " id = "navbar">
-         
+  <nav class="navbar navwidht navbar-collapse-xs navbar-toggler navbar-expand-lg " id = "navbar">  
          <a class="navbar-brand text" href="#" ></a>
-
          <ul class="navbar-nav mr-auto ">
            <li class="nav-item active">
              <a class="nav-link text" href="/">Accueil</a>
@@ -67,56 +64,51 @@
            <li class="nav-item">
                 <a class="nav-link text" href="/boutique">Boutique</a>
             </li>
-
-         </ul>
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Se connecter') }} </a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __("S'enregister") }}</a>
-                                </li>
-                            @endif
-                        @else
-                        @auth
-                        <div class="dropdown-menu" role="button" aria-labelledby="navbarDropdownMenuLink3">
-                        </div>
-                         @endauth
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink" id= 'navdown'>
-                                    <a class="dropdown-item" href="/logout">Déconnexion</a>
-                                    <a class="dropdown-item" href="">Notification</a>
-                                    <a class="dropdown-item" href="/basket">Panier</a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                  </ul>
-                </div>
-            </div>
-        </nav>
-
-        <div class="position-ref full-height">
-          <div class="content ">
-              <div class="m-t-md">
-                  @yield('content')
+         </ul> 
+          <ul class="navbar-nav ml-auto">     
+            @guest
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('login') }}">{{ __('Se connecter') }} </a>
+            </li>
+            @if (Route::has('register'))
+              <li class="nav-item">
+                  <a class="nav-link" href="{{ route('register') }}">{{ __("S'enregister") }}</a>
+              </li>
+            @endif
+            @else
+            @auth
+            <div class="dropdown-menu" role="button" aria-labelledby="navbarDropdownMenuLink3"></div>
+            @endauth
+            <li class="nav-item dropdown">
+              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }} <span class="caret"></span>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink" id= 'navdown'>
+                <a class="dropdown-item" href="/logout">Déconnexion</a>
+                <a class="dropdown-item" href="">Notification</a>
+                <a class="dropdown-item" href="/basket">Panier</a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+                </form>
               </div>
-          </div>
-        </div>
+            </li>
+            @endguest
+          </ul>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
-    <footer class="footer">
-        @yield('footer')
-        <footer class="page-footer font-small unique-color-dark">
+  <div class="full-height">
+    <div class="content">
+      <div class="m-t-md">
+        @yield('content')
+      </div>
+    </div>
+  </div>
+
+  
+        <footer class="page-footer font-small unique-color-dark footer">
             <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
               <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
               <script>
@@ -263,6 +255,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-    </body>
+</body>
    
 </html>
