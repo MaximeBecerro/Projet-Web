f0757462759d1f400c01715859a9b184dfd3183c
Yannis Taibi <yannis.taibi@viacesi.fr>
2019-01-24 10:04:13 +0100

    Cookies in template

--- Project_web/resources/views/master.blade.php
+++ Project_web/resources/views/master.blade.php
@@ -19,6 +19,28 @@
 
     </head>
     <body>
+      
+        <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
+        <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
+        @yield('cookies')
+        <script>
+        window.addEventListener("load", function(){
+        window.cookieconsent.initialise({
+          "palette": {
+            "popup": {
+              "background": "#000"
+            },
+            "button": {
+              "background": "#f1d600"
+            }
+          },
+          "showLink": false,
+          "content": {
+            "message": "Ce site utilise les cookies afin d'améliorer votre expérience de navigation.",
+            "dismiss": "Compris !"
+          }
+        })});
+        </script>
         <header class="header title flex-center row " >
             <div class="col-md-1.5"> 
                     <img src="{{ URL::to('/images/logo.png') }} ">
@@ -111,27 +133,6 @@
     <footer class="footer">
         @yield('footer')
         <footer class="page-footer font-small unique-color-dark">
-            <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
-              <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
-              <script>
-              window.addEventListener("load", function(){
-              window.cookieconsent.initialise({
-                "palette": {
-                  "popup": {
-                    "background": "#000"
-                  },
-                  "button": {
-                    "background": "#f1d600"
-                  }
-                },
-                "showLink": false,
-                "content": {
-                  "message": "Ce site utilise les cookies afin d'améliorer votre expérience de navigation.",
-                  "dismiss": "Compris !"
-                }
-              })});
-              </script>
-
 
                 <div style="background-color: #A9A9A9;">
                   <div class="container">
