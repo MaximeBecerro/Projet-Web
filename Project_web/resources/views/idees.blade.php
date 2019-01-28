4279957c59c6fbebca696a3cfcc626c565715f89
Yannis Taibi <yannis.taibi@viacesi.fr>
2019-01-28 16:30:54 +0100

    Auto stash before merge of "Branch1" and "origin/Branch1"

--- Project_web/resources/views/idees.blade.php
+++ Project_web/resources/views/idees.blade.php
@@ -5,6 +5,21 @@
 <!-- Modification du content -->
 
 <main role="main">
+    @php
+    $userId = Auth::id();
+    $user = Auth::user()->roleid;
+    @endphp
+    @if (Auth::check())
+    <form>
+        <div class="col-md-24 flex-center">
+            <div class="form-group">
+                <label for="description"></label>
+                <textarea type="text" class="form-control" id="description" rows="3" style="width: 400px;"> </textarea>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Proposer une idée</button>
+    </form>
+    @endif
     <div class="album py-5 ">
         <div class="container">
             <div class="row">
@@ -17,7 +32,7 @@
                     <div class="card-body">
                         <p class="card-text">' . $id['IdeaContent'] . '</p>
                         <a href="#" class="card-link">' . $id['id'] . '</a>
-                        <a href="#" class="card-link">Another link</a>
+                        <a href="#" class="card-link">' . $userId . '</a>
                     </div>
                 </div>';
                 }
